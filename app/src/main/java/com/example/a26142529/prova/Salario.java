a1e298bfb9d441413e45b81def2eb31bd56fa18c
JOAO VICTOR SELEIRO DE CARVALHO <26142529@sempreunama.com.br>
2019-04-05 20:59:10 -0300

    Initial commit

---
+++ app/src/main/java/com/example/a26142529/prova/Salario.java
@@ -0,0 +1,52 @@
+    package com.example.a26142529.prova;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+
+    public class Salario extends AppCompatActivity {
+
+    private TextInputEditText salario;
+    private RadioGroup percentual;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salario);
+
+        salario = findViewById(R.id.salario);
+        percentual = findViewById(R.id.rg);
+
+    }
+
+    public void calcular (View v){
+        double S = Double.parseDouble(salario.getText().toString());
+        int radio = percentual.getCheckedRadioButtonId();
+
+        if (radio == R.id.r4){
+            double r = (S *0.4) + S;
+            String r2 = "Seu novo salário é R$" + r;
+
+        }   else if (radio == R.id.r45){
+            double r = (S *0.45) + S;
+            String r2 = "Seu novo salário é R$" + r ;
+        }   else if (radio == R.id.r5){
+            double r = (S * 0.5) + S;
+            String r2 = "Seu novo salário é R$" + r ;
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+}
